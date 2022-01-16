1079e919c31f0ccb07210b1e8bd0c25eb24f9e3a
Denis Seleznev <hcodes@users.noreply.github.com>
2022-01-16 11:08:09 +0300

    Go!

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fast_average_color_1 = __importDefault(require("fast-average-color"));
+var fac = new fast_average_color_1.default();
+console.log(fac.getColor(null));
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import FastAverageColor from 'fast-average-color';
+
+const fac = new FastAverageColor();
+console.log(fac.getColor(null));
